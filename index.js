9d2d7decc24eb6a11766025f747044fc2cbbbeb4
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-11-15 21:16:23 +0000

    created connection with mongoDB and get data from there

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
+
 const app = express();
 
 /* middleware */
@@ -19,12 +20,31 @@ app.listen(port, () => {
 })
 
 
+/* mongodb connection */
+require('dotenv').config()
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mniec4l.mongodb.net/?retryWrites=true&w=majority`;
 
-
-const uri = "mongodb+srv://<username>:<password>@cluster0.mniec4l.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+
+/* mongodb CRUD operation */
+
+async function run() {
+    try {
+        /* create DB in mongoDB */
+        const appoinmentOptionsCollection = client.db('simora').collection('AppoinmentOptions');
+
+        /* (READ) get data from mongoDB server */
+        app.get('/AppoinmentOptions', async (req, res) => {
+            const query = {};
+            const options = await appoinmentOptionsCollection.find(query).toArray();
+            res.send(options);
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.log)
