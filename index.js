1d07050cd0924e2325cee52a1bef5423937e2a17
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-11-15 23:20:54 +0000

    (CREATE) create/get single data from client side and create a collection in mongoDB under initial DB from that data

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+/* API naming convention or (CRUD) operation for ex. booking collection:::::>>>>:::
+
+(READ) app.get('/booking') >>> to load all booking data
+(READ) app.get('/booking/:id) >>> to load a specific booking data
+(CREATE) app.post(/booking) >>> to add a new document/object inside booking collection
+(UPDATE) app.patch('/booking/:id) >>> to partially edit a specific booking data
+(UPDATE) app.put('/booking/:id) >>> to edit/replace a specific booking data
+(DELETE) app.delete('/booking/:id) >>> to delete a specific booking data
+*/
+
+
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
@@ -41,10 +52,19 @@ async function run() {
             res.send(options);
         })
 
+        /* (CREATE) create/get single data from client side and create a collection in mongoDB under initial DB from that data */
+        const bookingCollection = client.db('simora').collection('userBooking')
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body; //get booking data
+            console.log(booking);
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result)
+        })
     }
     finally {
 
     }
-}
 
+}
 run().catch(console.log)
