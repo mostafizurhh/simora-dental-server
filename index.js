f94b076338cfae8c50338bca499aca7b35c6f1f5
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-11-15 18:21:19 +0000

    basic server set up done

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+
+/* middleware */
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', async (req, res) => {
+    res.send('Server is running...........')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
+
+
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.mniec4l.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
